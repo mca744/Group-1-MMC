deda780e9980b32f683a4d270c90944ac46b6ab5
Megan A <79951477+mca744@users.noreply.github.com>
2023-06-24 21:00:12 -0400

    Update weatherAPI.py

--- scripts/weatherAPI.py
+++ scripts/weatherAPI.py
@@ -3,17 +3,17 @@ pip install requests
 #importing requests for API 
 import requests
 
-#sample latitude parameter 
+#sample latitude parameter - Flushing, NY
 lat_param = "40.77"  #insert "latitude" 
 
-#sample longitude parameter 
-long_param = "-73.78" #insert "longitude"
+#sample longitude parameter - Flushing, NY
+long_param = "-73.82" #insert "longitude"
 
 #utilizing API Url's 
 base_url ="https://archive-api.open-meteo.com/v1/archive?"
 
 #start date january 01, 2023 to june 01, 2023
-base_url2 = "&start_date=2023-01-01&end_date=2023-06-01&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation,rain,cloudcover,windspeed_10m,windspeed_100m,winddirection_10m,winddirection_100m&models=best_match&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&min=2022-06-01&max=2023-06-19"
+base_url2 = "&start_date=2023-01-01&end_date=2023-06-01&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation,rain,cloudcover,windspeed_10m,windspeed_100m,winddirection_10m,winddirection_100m&models=best_match&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&min=2023-01-01&max=2023-06-01"
 url = base_url + "latitude=" + lat_param + "&longitude=" + long_param + base_url2
 
 #print sample response 
